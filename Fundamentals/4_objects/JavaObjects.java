a5b0bb722e7108ba4cd1ebfabc5aa010713bf5ed
ST10090632 <ST10090632@vcconnect.edu.za>
2024-02-13 13:44:58 +0200

    JavaObjects

---
+++ Fundamentals/4_objects/JavaObjects.java
@@ -0,0 +1,16 @@
+public class JavaObjects{
+
+
+ public static void main(String[] args) {
+ 	   NamePrinter namePrinter = new NamePrinter();
+ 	   SurnamePrinter surnamePrinter = new SurnamePrinter();
+ 	  
+ 	   namePrinter.print();
+ 	   surnamePrinter.print();
+    }
+
+
+
+}
+
+
