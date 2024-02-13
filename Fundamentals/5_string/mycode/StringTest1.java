5e1e8aca590d45be7a40741a00f3ebd2b2e41234
ST10090632 <ST10090632@vcconnect.edu.za>
2024-02-13 13:58:59 +0200

    String Test 1

---
+++ Fundamentals/5_string/mycode/StringTest1.java
@@ -0,0 +1,9 @@
+public class StringTest1{
+    public static void main(String [] args){
+        String name = "Samantha";
+        String surname = "Figueiredo";
+		String fullName = name+" "+surname;
+
+        System.out.println("Full Name :"+fullName);
+    }
+}
