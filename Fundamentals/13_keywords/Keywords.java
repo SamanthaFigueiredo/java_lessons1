f82c60378250676aa700c334505e6cea206404a1
ST10090632 <ST10090632@vcconnect.edu.za>
2024-02-14 17:30:47 +0200

    keywords

---
+++ Fundamentals/13_keywords/Keywords.java
@@ -0,0 +1,20 @@
+public class Keywords{
+    public static void main(String [] args){
+        int myInt = 1;
+        boolean myBoolean = true;
+        double myDouble = 1.34;
+ 	   float myFloat = 31174.4F;
+        byte  myByte = 4;
+ 	   long myLong = 24;
+        short myShort = 7;
+        char myChar = 'A';
+ 	    System.out.println(myInt);
+        System.out.println(myBoolean);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myByte);
+        System.out.println(myLong);
+        System.out.println(myShort);
+        System.out.println(myChar);
+    }
+}
