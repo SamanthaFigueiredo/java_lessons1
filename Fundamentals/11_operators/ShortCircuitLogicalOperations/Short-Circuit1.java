6a247bfcbf649c81ac60b82dde28431bf764a337
ST10090632 <ST10090632@vcconnect.edu.za>
2024-02-15 12:00:29 +0200

    ShortCircuitLogicalOperations

---
+++ Fundamentals/11_operators/ShortCircuitLogicalOperations/Short-Circuit1.java
@@ -0,0 +1,8 @@
+public class Short-Circuit1 {
+    public static void main(String[] args) {
+ 	   boolean a = true;
+        boolean b = true;
+       
+        System.out.println(a && b);
+    }
+}
