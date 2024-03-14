ea2b7f6ea266dede4b8c746e7a7c77c36473ea44
ST10090632 <ST10090632@vcconnect.edu.za>
2024-03-14 11:07:03 +0200

    While

---
+++ Fundamentals/32_while/WhileTest.java
@@ -0,0 +1,12 @@
+public class WhileTest {
+    public static void main(String[] args) {
+        int counter = 0;      
+        while(counter < 10){
+
+          counter +=2;
+                 System.out.println("Counter = "+counter);
+
+           
+        }
+    }
+}
