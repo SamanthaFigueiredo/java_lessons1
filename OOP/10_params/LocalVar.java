45963aaa8ccfa257cd162fd3f9354919c598902d
ST10090632 <ST10090632@vcconnect.edu.za>
2024-05-10 15:45:01 +0200

    OOP

---
+++ OOP/10_params/LocalVar.java
@@ -0,0 +1,9 @@
+public class LocalVar {
+	public static void main(String[] args) {
+		int localint=10;
+		ValueHolder2 holder2 = new ValueHolder2();
+		
+		System.out.println(localint);
+		System.out.println(holder2.getValue());
+	}
+}
---
+++ OOP/10_params/PassByval2.java
@@ -0,0 +1,5 @@
+public class PassByval2 {
+	public void changeValue(int value){
+		value = 5000;
+	}
+}
